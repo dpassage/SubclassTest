29fb69bf7e3b0d18005c52fc6ae464fd3151b207
David Paschich <dpassage@balveda.com>
2014-12-14 15:06:41 -0800

    Now with Swift subclass in the test target. Fails to build if you include the class in the build.

---
+++ SubclassTestTests/FakeRatchet.swift
@@ -0,0 +1,15 @@
+//
+//  FakeRatchet.swift
+//  SubclassTest
+//
+//  Created by David Paschich on 12/14/14.
+//  Copyright (c) 2014 David Paschich. All rights reserved.
+//
+
+import Foundation
+
+import SubclassTest
+
+class FakeRatchet: Ratchet {
+
+}
