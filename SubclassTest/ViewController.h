2c71c5f31d1aad8414fc6260fc9f768413f990d0
David Paschich <dpassage@balveda.com>
2014-12-14 14:49:15 -0800

    Initial Commit

---
+++ SubclassTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SubclassTest
+//
+//  Created by David Paschich on 12/14/14.
+//  Copyright (c) 2014 David Paschich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
